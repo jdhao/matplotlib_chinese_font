d3c3547cf65551e9e33a38cf41f3f75af1f9b05e
jdhao <jdhao@hotmail.com>
2017-05-16 04:02:18 -0400

    Change the output font list to two columns

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-This repo is created as a guide on how to use Chinese font properly with Matplotlib.
-Simply run `python print_chinese.py`. To know more, please see the code for details.
+### Repo Description
+This repo is created as a demo on how to use Chinese fonts properly with Matplotlib.
+As different ways exist, you can choose whichever way you think best suit your need.
 
+### How to run the code
+use `python print_chinese.py --method method1` to run the code, valid method names are 'method1','method2' and 'method3'.
+
+### Find more info
 For more information, you can refer to [this post](https://jdhao.github.io/2017/05/13/guide-on-how-to-use-chinese-with-matplotlib/).
 
--- print_chinese.py
+++ print_chinese.py
@@ -5,16 +5,20 @@ import simplejson as json
 from find_zh_font import save_zh_font
 import os
 import argparse
+import math
+
+parser = argparse.ArgumentParser(description="Choose which method you want to use")
+parser.add_argument("--method",type=str, default="method1",choices=['method1', 'method2', 'method3'],
+                    help="method you want to use",)
+
+args = parser.parse_args()
+
 
 if os.path.isfile('valid_zh_font.json') and os.path.isfile('zh_font_pth_name.json'):
     pass
 else:
     save_zh_font()
 
-parser = argparse.ArgumentParser(description="Choose which method you want to use")
-parser.add_argument("--method",type=str, default="method1",choices=['method1', 'method2', 'method3'],
-                    help="method you want to use",)
-args = parser.parse_args()
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
@@ -24,22 +28,30 @@ ax.set_facecolor('#181A1C')
 mpl.rcParams['axes.unicode_minus'] = False
 alignment = {'horizontalalignment': 'left', 'verticalalignment': 'baseline'}
 default_size = fig.get_size_inches()
-
+fig.set_size_inches(default_size[0] * 1.5, default_size[1] * 2)
 
 if args.method == 'method1':
 
     with open('zh_font_pth_name.json', 'r') as f:
         font_info = json.load(f)
+
     size = len(font_info)
-    step = 1.0/size
-    fig.set_size_inches(default_size[0]*1, default_size[1]*2)
+    num_font_per_col = int(math.ceil(size/2))
+    step = 1.0/num_font_per_col
 
     print("{} fonts available".format(size))
     for i, (path, name) in enumerate(font_info):
 
         font = fm.FontProperties(fname=path)
-        ax.text(x=0.3,y=0.985-i*step, s=u'龙飞凤舞 ' + '(' + name + ')',
-                color='#FFFFFF', fontproperties=font, **alignment)
+
+        if i < num_font_per_col:
+            ax.text(x=0.1,y=0.97-i*step, s=u'龙飞凤舞 ' + '(' + name + ')',
+                    color='#FFFFFF', fontproperties=font, **alignment)
+        else:
+            j = i -num_font_per_col
+            ax.text(x=0.6, y=0.97-j*step, s=u'龙飞凤舞 ' + '(' + name + ')',
+                    color='#FFFFFF', fontproperties=font, **alignment)
+
     #plt.savefig('test.png',dpi='figure')
 
 if args.method == 'method2':
@@ -47,13 +59,21 @@ if args.method == 'method2':
         valid_zh_font = json.load(f)
 
     size = len(valid_zh_font)
-    step = 1.0/size
-    fig.set_size_inches(default_size[0], default_size[1]*2)
+    num_font_per_col = int(math.ceil(size / 2))
+    step = 1.0 / num_font_per_col
+
     print("{} fonts available".format(size))
 
     for i, name in enumerate(valid_zh_font):
-        ax.text(x= 0.3,y=0.97-i*step, s=u'龙飞凤舞 '+"(" + name+")",
+
+        if i < num_font_per_col:
+            ax.text(x= 0.1,y=0.95-i*step, s=u'龙飞凤舞 '+"(" + name+")",
                     color='#FFFFFF', fontname=name, **alignment)
+        else:
+            j = i-num_font_per_col
+            ax.text(x=0.6, y=0.95 - j* step, s=u'龙飞凤舞 ' + "(" + name + ")",
+                    color='#FFFFFF', fontname=name, **alignment)
+
 
 if args.method == 'method3':
 
@@ -65,16 +85,20 @@ if args.method == 'method3':
                          "valid_zh_font num is {}".format(len(valid_zh_font)))
 
     size = len(valid_zh_font)
-    step = 1.0 / size
-    fig.set_size_inches(default_size[0], default_size[1] * 2)
+    num_font_per_col = int(math.ceil(size / 2))
+    step = 1.0 / num_font_per_col
     print("{} fonts available".format(size))
 
     for i, name in enumerate(valid_zh_font):
         mpl.rcParams['font.family']= name
         # mpl.rcParams['font.family']= 'sans-serif'
         # mpl.rcParams['font.sans-serif'] = name
-
-        ax.text(x=0.3, y=0.97 - i * step, s=u'龙飞凤舞 ' + "(" + name + ")",
-                color='#FFFFFF', **alignment)
+        if i < num_font_per_col:
+            ax.text(x=0.1, y=0.95 - i * step, s=u'龙飞凤舞 ' + "(" + name + ")",
+                    color='#FFFFFF', **alignment)
+        else:
+            j = i-num_font_per_col
+            ax.text(x=0.6, y=0.95 - j* step, s=u'龙飞凤舞 ' + "(" + name + ")",
+                    color='#FFFFFF', **alignment)
 
 plt.show()
